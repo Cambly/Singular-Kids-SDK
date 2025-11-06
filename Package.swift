949ac8b540f9d049bb6a86feae46baf5c0a8b038
Tim DeGraw <tedegraw@gmail.com>
2025-11-05 14:14:42 -1000

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
     products: [
         .library(
             name: "SingularKids",
-            targets: ["Singular"]),
+            targets: ["SingularKids"]),
     ],
     dependencies: [
     ],
